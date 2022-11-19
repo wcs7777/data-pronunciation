22f008329285b2129e82a84fb29d99a6f933778a
Willian Carlos <wcs777git@outlook.com>
2022-11-19 00:09:19 -0300

    update update.sh

--- update.sh
+++ update.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-mv ~/win/Downloads/pronunciation-audio-table.json audio.table.json
-mv ~/win/Downloads/pronunciation-ipa-table.json ipa.table.json
-awk '!/: "s",/' audio.table.json > tmp.json && mv tmp.json audio.table.json
+audio_file="./audio.table.json"
+ipa_file="./ipa.table.json"
+cp ~/win/Downloads/pronunciation-audio-table.json $audio_file
+cp ~/win/Downloads/pronunciation-ipa-table.json $ipa_file
+awk '!/: "s",/' $audio_file > tmp.json && mv tmp.json $audio_file
+node sort.js $audio_file
+node sort.js $ipa_file
