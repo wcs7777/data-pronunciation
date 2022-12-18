4fe579ab5aae10ba7a5e892aa5f805da5e73635e
Willian Carlos <wcs777git@outlook.com>
2022-12-18 07:24:40 -0300

    updated on 12/18/2022

--- update.sh
+++ update.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
-audio_file="./audio.table.json"
-ipa_file="./ipa.table.json"
-cp ~/win/Downloads/pronunciation-audio-table.json $audio_file
-cp ~/win/Downloads/pronunciation-ipa-table.json $ipa_file
-awk '!/: "s",/' $audio_file > tmp.json && mv tmp.json $audio_file
-node sort.js $audio_file
-node sort.js $ipa_file
+__wcs_pronunciation_update() {
+	local audio_files
+	local ipa_files
+	local files_path
+	audio_file="./audio.table.json"
+	ipa_file="./ipa.table.json"
+	files_path="/mnt/c/Users/wcs/Downloads"
+	cp -v "$files_path/pronunciation-audio-table.json" $audio_file
+	cp -v "$files_path/pronunciation-ipa-table.json" $ipa_file
+	awk '!/: "s",/' $audio_file > tmp.json && mv tmp.json $audio_file
+	node sort.js $audio_file
+	node sort.js $ipa_file
+}
+
+__wcs_pronunciation_update
+unset -f __wcs_pronunciation_update
